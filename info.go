ccf86fec7fefb5f79e0cb20d10ce9e98071db9b6
Benjamin Guillet <benjamin.guillet@gmail.com>
2015-07-27 14:32:38 -0700

    Just display app.Name and app.Id for now.

--- info.go
+++ info.go
@@ -21,10 +21,6 @@ func runInfo(cmd *Command, args []string) {
 	}
 	app, err := client.AppInfo(mustApp())
 	must(err)
-	fmt.Printf("Name:     %s\n", app.Name)
-	fmt.Printf("Owner:    %s\n", app.Owner.Email)
-	fmt.Printf("Region:   %s\n", app.Region.Name)
-	fmt.Printf("Stack:    %s\n", app.Stack.Name)
-	fmt.Printf("Git URL:  %s\n", app.GitURL)
-	fmt.Printf("Web URL:  %s\n", app.WebURL)
+	fmt.Printf("Name: %s\n", app.Name)
+	fmt.Printf("ID:   %s\n", app.Id)
 }
